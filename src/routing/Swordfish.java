9d0d51015954f951a87da5a9d529b4a05de30230
diogo <diogosoaresm@ufam.edu.br>
2019-02-28 01:18:15 -0400

    Add Swordfish strategy

--- src/routing/Swordfish.java
+++ src/routing/Swordfish.java
@@ -1,6 +1,7 @@
 package routing;
 
 import core.*;
+import edu.uci.ics.jung.graph.util.Pair;
 import util.Tuple;
 
 import java.io.File;
@@ -38,28 +39,61 @@ public class Swordfish extends ActiveRouter {
     }
 
     public void tryOtherMessages(){
-        /*
-        List<Connection> connections = getConnections();
-        if (connections.size() == 0 || this.getNrofMessages() == 0) {
-            return;
-        }
+        //nao tenho mensagens pra enviar ou nao tenho vizinhos
+        if(this.getNrofMessages() == 0 || getConnections().size() <= 0) return;
 
-        Collection<Message> msgCollection = getMessageCollection();
+        ArrayList<Tuple<Double, Tuple<DTNHost, Connection>>> vizinhos = new ArrayList<Tuple<Double, Tuple<DTNHost, Connection>>>();
         List<Tuple<Message, Connection>> messages = new ArrayList<Tuple<Message, Connection>>();
 
+        //Debug.p("Eu sou " + this.getHost());
+        //Debug.p("Minhas mensagens " + getMessageCollection());
+        //Debug.p("Meus vizinhos: ");
         for(Connection con : getConnections()){
-            DTNHost vizinho = con.getOtherNode(this.getHost());
+            DTNHost viz = con.getOtherNode(this.getHost());
+            //Debug.p("\t" + viz + " ProbForwarding: " + World.PforwardS_i.get(viz.getAddress()));
+            //Debug.p("\t\t" + World.B_i.get(viz.getAddress()));
 
-            //ele é meu amigo!!!
-            if(grafoAmizade.get(vizinho.getAddress()) == 1){
-                for(Message m : msgCollection){
-                    messages.add(new Tuple<Message, Connection>(m, con));
-                }
+            vizinhos.add(new Tuple<Double, Tuple<DTNHost, Connection>>(World.PforwardS_i.get(viz.getAddress()),
+                    new Tuple<DTNHost, Connection>(viz, con)));
+        }
+
+        /*
+        Debug.p("\nTupla::");
+        for(Tuple<Double, Tuple<DTNHost, Connection>> t : vizinhos){
+            Debug.p("\t" + t.getKey() + ", " + t.getValue());
+        }
+        Debug.p("\n\n");
+        */
+
+        Comparator<Tuple<Double, Tuple<DTNHost, Connection>>> comparator = new Comparator<Tuple<Double, Tuple<DTNHost, Connection>>>() {
+            public int compare(Tuple<Double, Tuple<DTNHost, Connection>> tupleA, Tuple<Double, Tuple<DTNHost, Connection>> tupleB) {
+                if(tupleA.getKey() < tupleB.getKey()) return 1;
+                return -1;
+            }
+        };
+
+        Collections.sort(vizinhos, comparator);
+
+        /*
+        Debug.p("\nTupla depois do sort::");
+        for(Tuple<Double, Tuple<DTNHost, Connection>> t : vizinhos){
+            Debug.p("\t" + t.getKey() + ", " + t.getValue());
+        }
+        Debug.p("\n\n");
+        */
+
+        /*
+         * Envia ordenado pras conexoes que tem maior prob forwarding
+         */
+        for(Tuple<Double, Tuple<DTNHost, Connection>> t : vizinhos){
+            Connection con = t.getValue().getValue();
+
+            for(Message m : this.getMessageCollection()){
+                messages.add(new Tuple<Message, Connection>(m, con));
             }
         }
 
         tryMessagesForConnected(messages);
-        */
     }
 
     @Override
