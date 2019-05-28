3111e818a74caee599157c6711b60dc97f339af6
diogosm <dsmozh@gmail.com>
2019-05-28 15:20:19 -0400

    memory starvation

--- one.sh
+++ one.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-java -Xmx512M -Djava.util.Arrays.useLegacyMergeSort=true -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar:lib/jung-jai-2.0.1.jar:lib/jung-graph-impl-2.0.1.jar:lib/gs-core-1.3.jar:lib/gs-algo-1.3.jar core.DTNSim $*
+#java -Xmx1024M -Djava.util.Arrays.useLegacyMergeSort=true -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar:lib/jung-jai-2.0.1.jar:lib/jung-graph-impl-2.0.1.jar:lib/gs-core-1.3.jar:lib/gs-algo-1.3.jar core.DTNSim $*
+java -Xmx1024M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar:lib/jung-jai-2.0.1.jar:lib/jung-graph-impl-2.0.1.jar:lib/gs-core-1.3.jar:lib/gs-algo-1.3.jar core.DTNSim $*
